6201c58685d18f785220981fa02bdd623eeb6098
mody-sahariar1 <sahariar.mody567@gmail.com>
2025-12-19 17:52:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const player = document.getElementById("player");
+const playBtn = document.getElementById("play-btn");
+
+let x = 0;
+let speed = 2;
+let direction = 1;
+let isPlaying = false;
+
+function animate() {
+  if (!isPlaying) return;
+
+  x += speed * direction;
+
+  if (x >= 560 || x <= 0) {
+    direction *= -1;
+  }
+
+  player.style.left = x + "px";
+  requestAnimationFrame(animate);
+}
+
+playBtn.addEventListener("click", () => {
+  if (!isPlaying) {
+    isPlaying = true;
+    animate();
+  }
+});
